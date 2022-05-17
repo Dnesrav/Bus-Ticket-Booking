d4d4aefa0350620ceeb5ae17d72d786659b18720
Dnesrav <65590707+Dnesrav@users.noreply.github.com>
2022-05-17 18:35:14 +0530

    Add files via upload

---
+++ RegisterTest.java
@@ -0,0 +1,14 @@
+package Reg_Controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RegisterTest {
+	Register obj;
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
